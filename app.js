399ad99d6cae15ae0802598af9efd04bcf9d3333
Watcharaphol D <dangpiem@hotmail.com>
2024-07-12 14:55:17 +0700

    Create logic for checking design-token

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// write code here
+const designToken = {
+    'color-primary-500': '#3498db',
+    'color-secondary-500': '#2ecc71',
+    'text-color': '#333333',
+    'font-base': '16px',
+    'font-weight-light': '300',
+    'font-weight-regular': '400',
+    'font-weight-bold': '700',
+    'spacing-small': '8px',
+    'spacing-medium': '16px',
+    'spacing-large': '24px',
+    'border-radius-small': '4px',
+    'border-radius-medium': '8px',
+    'border-radius-large': '12px',
+}
+
+let userToken = prompt("Please enter the required token");
+if (designToken[userToken] !== undefined) {
+    alert(`${userToken} : ${designToken[userToken]}`);
+} else {
+    alert(`Design token "${userToken}" not found.`);
+}
